63cb855fd8fdeb56eff03ae3328d0c73f382e71a
nandish <pnandish122@gmail.comgit config --global user.email pnandish122@gmail.com>
2024-11-20 13:47:45 +0530

    python assignment

---
+++ modules/sample.py
@@ -0,0 +1,16 @@
+from calci import add, sub
+from calci import *
+
+re=div(10,5)
+print(re)
+
+
+
+result=add(20, 30)
+print(result)
+
+result =sub(40,20)
+print(result)
+
+result=mul(2,5)
+print(result)
